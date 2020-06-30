05fdca4ce5cb1ceb38a2db4cc00ca53ab6f7da5d
meiye <mabel.yemei@gmail.com>
2020-06-30 16:41:26 -0400

    init project

---
+++ src/test/java/site/meiye/SpringMvcDemo/SpringMvcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package site.meiye.SpringMvcDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
